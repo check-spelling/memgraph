a3aff4f001eed56064031f63a9e9fef979802200
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-14 10:08:17 -0400

    spelling: unopened
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/utils/file.cpp
+++ src/utils/file.cpp
@@ -502,7 +502,7 @@ void OutputFile::Close() noexcept {
 }
 
 void OutputFile::FlushBuffer(bool force_flush) {
-  MG_ASSERT(IsOpen(), "Flushing an unopend file.");
+  MG_ASSERT(IsOpen(), "Flushing an unopened file.");
 
   if (!force_flush && buffer_position_.load() < kFileBufferSize) return;
 
